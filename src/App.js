f8859e4276ca2df5c2fbb31e07331451141c1eec
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2020-05-21 10:16:28 +0530

    Signup added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Login from './components/auth/login'
-
-function App() {
-  return (
-    <div>
-       <Login />
-    </div>
-  )
+import React from 'react';
+import PageSpinner from './components/PageSpinner';
+import { BrowserRouter,  Route, Switch } from 'react-router-dom';
+ 
+const SignIn = React.lazy(() => import('./components/auth/login'));
+const SignUp = React.lazy(() => import('./components/auth/SignUp'));
+ 
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+          <Switch>
+              <React.Suspense fallback={<PageSpinner />}>
+                <Route exact path="/" component={SignIn} />
+                <Route exact path="/login" component={SignIn} />
+                <Route exact path="/signup" component={SignUp} />
+              </React.Suspense>
+          </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App
+export default App;
