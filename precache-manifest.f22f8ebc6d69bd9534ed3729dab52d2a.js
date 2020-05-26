96ea9de0bbfd3f071d7210fb7b017eeacddc55c8
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2020-05-26 18:31:12 +0530

    Updates

---
+++ precache-manifest.f22f8ebc6d69bd9534ed3729dab52d2a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2de5b724a5a2bbbfc1db04ae3c9569a",
+    "url": "/react-registration/index.html"
+  },
+  {
+    "revision": "62a28ae230b63377ee12",
+    "url": "/react-registration/static/js/0.0667f054.chunk.js"
+  },
+  {
+    "revision": "4c7b630b3045d180fd1e",
+    "url": "/react-registration/static/js/1.697461aa.chunk.js"
+  },
+  {
+    "revision": "9c86b545821737ad39be",
+    "url": "/react-registration/static/js/4.4910598b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-registration/static/js/4.4910598b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c082aba4f785e800b430",
+    "url": "/react-registration/static/js/5.542a64f8.chunk.js"
+  },
+  {
+    "revision": "034f425761dec0a13908",
+    "url": "/react-registration/static/js/6.da7b3db5.chunk.js"
+  },
+  {
+    "revision": "ce79f332adaa52623694",
+    "url": "/react-registration/static/js/7.07eb9d74.chunk.js"
+  },
+  {
+    "revision": "fad06b3f2397c0b849ae",
+    "url": "/react-registration/static/js/8.e0a26784.chunk.js"
+  },
+  {
+    "revision": "1b73150c41476e81d75d",
+    "url": "/react-registration/static/js/main.c278053d.chunk.js"
+  },
+  {
+    "revision": "c898159847fc34a9ca02",
+    "url": "/react-registration/static/js/runtime-main.27079c60.js"
+  }
+]);
